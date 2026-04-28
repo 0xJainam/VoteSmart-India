2e28f5df673ff2171b3cdf8181cc7d36c04e24dd
0xJainam <jainamsonigra@gmail.com>
2026-04-28 18:51:06 +0530

    refactor: hide firebase config into environment variables for security

--- lib/firebaseClient.ts
+++ lib/firebaseClient.ts
@@ -2,13 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy5QtF1cL62qJEg7ZzvqzYl9F6DGxWucw",
-  authDomain: "votesmart-india-78563.firebaseapp.com",
-  projectId: "votesmart-india-78563",
-  storageBucket: "votesmart-india-78563.firebasestorage.app",
-  messagingSenderId: "439362404762",
-  appId: "1:439362404762:web:13449e465013c7f2dbba23",
-  measurementId: "G-ER4E8CFHWN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export function getFirebaseApp() {
